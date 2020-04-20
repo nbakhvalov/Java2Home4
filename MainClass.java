5de15c6186a39b4e7a41e3a7b32685a84568fd29
Nikolay Bakhvalov <61291119+nbakhvalov@users.noreply.github.com>
2020-04-20 15:20:59 +0300

    Add files via upload

--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,10 @@
 package ru.geekbrains.java2.hw4;
-
+/*
+*
+*  author Nikolay Bakhvalov
+*  time 2020-04-20
+*
+ */
 public class MainClass {
     public static void main(String[] args) {
         new MyWindow();
